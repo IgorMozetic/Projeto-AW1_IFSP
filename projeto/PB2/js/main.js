204664582bfb30c8bce2d5c9dc822b7bdb410fbc
Igor Mozetic <imozetic@blueedtech.com.br>
2021-08-02 21:58:37 -0300

    site menu

---
+++ projeto/PB2/js/main.js
@@ -0,0 +1,27 @@
+// const scrollReveal = ScrollReveal({
+//     origin: 'top',
+//     distance: '30px',
+//     duration: 700,
+//     reset: true
+// })
+
+// scrollReveal.reveal(
+//     `#pag1 .inicio, #pag1 .text,
+//     #about .image, #about .text,
+//     #pag3 img_pag_impar, #services .card,
+//     #testimonials header, #testimonials .testimonials
+//     #contact .text, #contact .links,
+//     footer .brand, footer .social
+//     `,
+//     { interval: 100 }
+// )
+
+// const backToTopButton = document.querySelector('.back-to-top')
+
+// function backToTop() {
+//     if (window.scrollY >= 560) {
+//         backToTopButton.classList.add('show')
+//     } else {
+//         backToTopButton.classList.remove('show')
+//     }
+// }
