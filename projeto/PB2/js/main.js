b5565906ca74b96ac9c1a4606b2db21569422b41
Igor Mozetic <imozetic@blueedtech.com.br>
2021-08-03 10:48:50 -0300

    Alteração back-to-top

--- projeto/PB2/js/main.js
+++ projeto/PB2/js/main.js
@@ -16,12 +16,12 @@
 //     { interval: 100 }
 // )
 
-// const backToTopButton = document.querySelector('.back-to-top')
+const backToTopButton = document.querySelector('.back-to-top')
 
-// function backToTop() {
-//     if (window.scrollY >= 560) {
-//         backToTopButton.classList.add('show')
-//     } else {
-//         backToTopButton.classList.remove('show')
-//     }
-// }
+function backToTop() {
+    if (window.scrollY >= 560) {
+        backToTopButton.classList.add('show')
+    } else {
+        backToTopButton.classList.remove('show')
+    }
+}
