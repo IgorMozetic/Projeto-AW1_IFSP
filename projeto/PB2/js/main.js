2579e8c71ad98fd4a54b2d87adeed7d3d620d226
Igor Mozetic <imozetic@blueedtech.com.br>
2021-08-04 18:47:32 -0300

    Alteração de botao topo e mexida no js

--- projeto/PB2/js/main.js
+++ projeto/PB2/js/main.js
@@ -1,3 +1,15 @@
+const backToTopButton = document.querySelector('.back-to-top')
+
+window.addEventListener('scroll', function () {
+    if (window.scrollY >= 560) {
+        // scroll é maior que a altura do header
+        backToTopButton.classList.add('show')
+    } else {
+        // menor que a altura do header
+        backToTopButton.classList.remove('show')
+    }
+})
+
 const scrollReveal = ScrollReveal({
     origin: 'top',
     distance: '30px',
@@ -6,21 +18,12 @@ const scrollReveal = ScrollReveal({
 })
 
 scrollReveal.reveal(
-    `#pag1 .inicio, #pag1 #title, #pag1 #subtitle, .page-logo,
-    .title_pags, .texto-imagem, .img_pag_par, .img_pag_impar,
-    .texto_par_par, .texto_par_impar, .img_pag_meio, .redes_sociais,
-    .rodape,
+    `#pag1 #title, #pag1 #subtitle, .page-logo, .inicio,
+    .title_pags, .texto-imagem, .img_pag_par, .texto_par_par,
+    .img_pag_meio, .img_pag_impar, .texto_par_impar, 
+    .redes_sociais, .rodape,
     .label_input, .formulario, .btn, #title, #lista1
     `,
     { interval: 100 }
 )
 
-const backToTopButton = document.querySelector('.back-to-top')
-
-function backToTop() {
-    if (window.scrollY >= 560) {
-        backToTopButton.classList.add('show')
-    } else {
-        backToTopButton.classList.remove('show')
-    }
-}
