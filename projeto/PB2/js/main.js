0105e09079212ea054cb8ff4af2e83ef15ef1c06
Igor Mozetic <imozetic@blueedtech.com.br>
2021-08-04 14:42:02 -0300

    root e js de aparecer

--- projeto/PB2/js/main.js
+++ projeto/PB2/js/main.js
@@ -1,20 +1,19 @@
-// const scrollReveal = ScrollReveal({
-//     origin: 'top',
-//     distance: '30px',
-//     duration: 700,
-//     reset: true
-// })
+const scrollReveal = ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 500,
+    reset: true
+})
 
-// scrollReveal.reveal(
-//     `#pag1 .inicio, #pag1 .text,
-//     #about .image, #about .text,
-//     #pag3 img_pag_impar, #services .card,
-//     #testimonials header, #testimonials .testimonials
-//     #contact .text, #contact .links,
-//     footer .brand, footer .social
-//     `,
-//     { interval: 100 }
-// )
+scrollReveal.reveal(
+    `#pag1 .inicio, #pag1 #title, #pag1 #subtitle, .page-logo,
+    .title_pags, .texto-imagem, .img_pag_par, .img_pag_impar,
+    .texto_par_par, .texto_par_impar, .img_pag_meio, .redes_sociais,
+    .rodape,
+    .label_input, .formulario, .btn, #title, #lista1
+    `,
+    { interval: 100 }
+)
 
 const backToTopButton = document.querySelector('.back-to-top')
 
