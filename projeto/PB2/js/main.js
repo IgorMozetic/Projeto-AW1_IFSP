5e0530d18663b402adcd183d2993bbf00e0ce110
Igor Mozetic <imozetic@blueedtech.com.br>
2021-08-07 22:36:50 -0300

    responsividade e menu sanduiche

--- projeto/PB2/js/main.js
+++ projeto/PB2/js/main.js
@@ -1,7 +1,7 @@
 const backToTopButton = document.querySelector('.back-to-top')
 
 window.addEventListener('scroll', function () {
-    if (window.scrollY >= 560) {
+    if (window.scrollY >= 360 && window.scrollY <= 2500) {
         // scroll é maior que a altura do header
         backToTopButton.classList.add('show')
     } else {
@@ -21,8 +21,24 @@ scrollReveal.reveal(
     `#pag1 #title, #pag1 #subtitle, .page-logo, .inicio,
     .title_pags, .texto-imagem, .img_pag_par, .texto_par_par,
     .img_pag_meio, .img_pag_impar, .texto_par_impar,
-    .label_input, .formulario, .btn, #title, #lista1
+    .label_input, .formulario, .btn, #title
     `,
     { interval: 70 }
 )
 
+const nav = document.querySelector('header nav')
+const toggle = document.querySelectorAll('div.toggle')
+
+for (const element of toggle) {
+    element.addEventListener('click', function () {
+        nav.classList.toggle('show')
+    })
+}
+
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+    link.addEventListener('click', function () {
+        nav.classList.remove('show')
+    })
+}
