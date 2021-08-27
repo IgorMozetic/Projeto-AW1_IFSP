84d35fbf8c17db67a4a5d3daf9479f70f61b608d
Igor Mozetic <71463737+IgorMozetic@users.noreply.github.com>
2021-08-27 20:58:08 -0300

    A11

--- Atividades/A11/Level7.js
+++ Atividades/A11/Level7.js
@@ -1,15 +1,10 @@
 function init(robot) {
     console.log("Robot starting")
-    robot.oldCoins = 0;
-    robot.oldEnergy = 100;
 }
 
 function loop(robot) {
-    robot.action = { type: 'move', amount: 40 };
-    if (robot.info().coins > robot.oldCoins ||
-        robot.info().energy > robot.oldEnergy) {
+   robot.action = { type: 'move', amount: 40 };
+   if ((200 <= robot.info().x && robot.info().x <= 300)) {
         robot.action = { type: 'jump', amount: 10 };
     }
-    robot.oldCoins = robot.info().coins;
-    robot.oldEnergy = robot.info().energy;
 }
